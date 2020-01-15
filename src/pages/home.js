2ef956582ac05fd666e5d2e1c3f0e81865556a39
scsin <saracsin26@gmail.com>
2020-01-15 14:52:31 -0300

    function(Calculate > valueDolarCI com IOF

--- src/pages/home.js
+++ src/pages/home.js
@@ -56,7 +56,7 @@ class Home extends React.Component {
 
     Calculate = () => {
         const valueDolarSI = parseFloat(this.state.dolarValue);
-        const valueDolarCI = ((parseFloat(this.state.dolarValue)) + ((parseFloat(this.state.dolarValue)) * (parseFloat(this.state.stateRate)/100))).toFixed(4);
+        const valueDolarCI = (((parseFloat(this.state.dolarValue)) + ((parseFloat(this.state.dolarValue)) * (parseFloat(this.state.stateRate)/100))) + (parseFloat(this.state.dolarValue) * parseFloat(this.state.valueIOF))).toFixed(4);
         const valueRealSI = (parseFloat(this.state.dolarValue) * parseFloat(this.state.price)).toFixed(4);
         const valueRealCI = ((((parseFloat(this.state.dolarValue)) + (parseFloat(this.state.dolarValue) * (parseFloat(this.state.stateRate)/100))) * (parseFloat(this.state.price))) + (parseFloat(this.state.dolarValue) * parseFloat(this.state.valueIOF))).toFixed(4);
 
